fc0aead655faee21eb360b9eb14503eb2caaa89b
Abdullah Abbasi <abdullaahabbasi@gmail.com>
2023-11-05 21:11:53 +0500

    Fixed your issues

--- Frontend/src/screens/home/Home.js
+++ Frontend/src/screens/home/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
-import Login from "../login/Login";
+import React, { useState } from 'react';
+import { Tabs, Tab } from '@material-ui/core';
+import DoctorList from '../doctorList/DoctorList';
+import Appointment from '../appointment/Appointment';
 
 const Home = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
   return (
     <div>
+      <Tabs value={selectedTab} onChange={handleTabChange} variant="fullWidth">
+        <Tab label="DOCTORS" />
+        <Tab label="APPOINTMENT" />
+      </Tabs>
+      {selectedTab === 0 && <DoctorList />} {/* Doctors go here */}
+      {selectedTab === 1 && <Appointment />} {/* apps go here*/}
     </div>
   );
 };
